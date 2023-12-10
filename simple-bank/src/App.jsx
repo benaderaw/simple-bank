01eff933f6a311a872b7e0fa5d8667190101d2d4
Ben Aderaw <asyncba@gmail.com>
2023-12-10 02:22:25 -0500

    implemented deposit feature

--- simple-bank/src/App.jsx
+++ simple-bank/src/App.jsx
@@ -8,13 +8,15 @@ const initialState = {
   isDisabled: true,
 };
 
+const DEPOSIT_AMOUNT = 150;
+
 const reducer = function (state, action) {
   //
   switch (action.type) {
     case "openAccount":
       return { ...state, balance: 500, isDisabled: false };
-    case "hola":
-      return { ...state, balance: 5000 };
+    case "deposit":
+      return { ...state, balance: state.balance + DEPOSIT_AMOUNT };
 
     default:
       return {};
@@ -39,7 +41,13 @@ function App() {
         Open account
       </button>
 
-      <button disabled={isDisabled}>Deposit</button>
+      <button
+        disabled={isDisabled}
+        onClick={() => dispatch({ type: "deposit" })}
+      >
+        Deposit {DEPOSIT_AMOUNT}
+      </button>
+
       <button disabled={isDisabled}>withdraw</button>
       <button disabled={isDisabled}>request a loan of 5000</button>
       <button disabled={isDisabled}>pay loan</button>
